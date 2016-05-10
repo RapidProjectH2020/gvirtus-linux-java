523aedeab753f9e0e20af37315148bb1ff2f50bb
Carmine Ferraro <carmine.ferraro@uniparthenope.it>
2016-05-10 16:31:13 +0200

    Bugfix

--- JavaFE/src/gvirtusfe/CudaRt_device.java
+++ JavaFE/src/gvirtusfe/CudaRt_device.java
@@ -328,7 +328,7 @@ public class CudaRt_device {
     }
     
     public class cudaDeviceProp{
-        
+
         String name;
         long totalGlobalMem=0;             /**< Global memory available on device in bytes */
         long sharedMemPerBlock=0;          /**< Shared memory available per block in bytes */
@@ -393,5 +393,508 @@ public class CudaRt_device {
         int    multiGpuBoardGroupID=0;       /**< Unique identifier for a group of devices on the same multi-GPU board */
           
     public cudaDeviceProp() {}
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public long getTotalGlobalMem() {
+            return totalGlobalMem;
+        }
+
+        public void setTotalGlobalMem(long totalGlobalMem) {
+            this.totalGlobalMem = totalGlobalMem;
+        }
+
+        public long getSharedMemPerBlock() {
+            return sharedMemPerBlock;
+        }
+
+        public void setSharedMemPerBlock(long sharedMemPerBlock) {
+            this.sharedMemPerBlock = sharedMemPerBlock;
+        }
+
+        public int getRegsPerBlock() {
+            return regsPerBlock;
+        }
+
+        public void setRegsPerBlock(int regsPerBlock) {
+            this.regsPerBlock = regsPerBlock;
+        }
+
+        public int getWarpSize() {
+            return warpSize;
+        }
+
+        public void setWarpSize(int warpSize) {
+            this.warpSize = warpSize;
+        }
+
+        public long getMemPitch() {
+            return memPitch;
+        }
+
+        public void setMemPitch(long memPitch) {
+            this.memPitch = memPitch;
+        }
+
+        public int getMaxThreadsPerBlock() {
+            return maxThreadsPerBlock;
+        }
+
+        public void setMaxThreadsPerBlock(int maxThreadsPerBlock) {
+            this.maxThreadsPerBlock = maxThreadsPerBlock;
+        }
+
+        public int[] getMaxThreadsDim() {
+            return maxThreadsDim;
+        }
+
+        public void setMaxThreadsDim(int[] maxThreadsDim) {
+            this.maxThreadsDim = maxThreadsDim;
+        }
+
+        public int[] getMaxGridSize() {
+            return maxGridSize;
+        }
+
+        public void setMaxGridSize(int[] maxGridSize) {
+            this.maxGridSize = maxGridSize;
+        }
+
+        public int getClockRate() {
+            return clockRate;
+        }
+
+        public void setClockRate(int clockRate) {
+            this.clockRate = clockRate;
+        }
+
+        public long getTotalConstMem() {
+            return totalConstMem;
+        }
+
+        public void setTotalConstMem(long totalConstMem) {
+            this.totalConstMem = totalConstMem;
+        }
+
+        public int getMajor() {
+            return major;
+        }
+
+        public void setMajor(int major) {
+            this.major = major;
+        }
+
+        public int getMinor() {
+            return minor;
+        }
+
+        public void setMinor(int minor) {
+            this.minor = minor;
+        }
+
+        public long getTextureAlignment() {
+            return textureAlignment;
+        }
+
+        public void setTextureAlignment(long textureAlignment) {
+            this.textureAlignment = textureAlignment;
+        }
+
+        public long getTexturePitchAlignment() {
+            return texturePitchAlignment;
+        }
+
+        public void setTexturePitchAlignment(long texturePitchAlignment) {
+            this.texturePitchAlignment = texturePitchAlignment;
+        }
+
+        public int getDeviceOverlap() {
+            return deviceOverlap;
+        }
+
+        public void setDeviceOverlap(int deviceOverlap) {
+            this.deviceOverlap = deviceOverlap;
+        }
+
+        public int getMultiProcessorCount() {
+            return multiProcessorCount;
+        }
+
+        public void setMultiProcessorCount(int multiProcessorCount) {
+            this.multiProcessorCount = multiProcessorCount;
+        }
+
+        public int getKernelExecTimeoutEnabled() {
+            return kernelExecTimeoutEnabled;
+        }
+
+        public void setKernelExecTimeoutEnabled(int kernelExecTimeoutEnabled) {
+            this.kernelExecTimeoutEnabled = kernelExecTimeoutEnabled;
+        }
+
+        public int getIntegrated() {
+            return integrated;
+        }
+
+        public void setIntegrated(int integrated) {
+            this.integrated = integrated;
+        }
+
+        public int getCanMapHostMemory() {
+            return canMapHostMemory;
+        }
+
+        public void setCanMapHostMemory(int canMapHostMemory) {
+            this.canMapHostMemory = canMapHostMemory;
+        }
+
+        public int getComputeMode() {
+            return computeMode;
+        }
+
+        public void setComputeMode(int computeMode) {
+            this.computeMode = computeMode;
+        }
+
+        public int getMaxTexture1D() {
+            return maxTexture1D;
+        }
+
+        public void setMaxTexture1D(int maxTexture1D) {
+            this.maxTexture1D = maxTexture1D;
+        }
+
+        public int getMaxTexture1DMipmap() {
+            return maxTexture1DMipmap;
+        }
+
+        public void setMaxTexture1DMipmap(int maxTexture1DMipmap) {
+            this.maxTexture1DMipmap = maxTexture1DMipmap;
+        }
+
+        public int getMaxTexture1DLinear() {
+            return maxTexture1DLinear;
+        }
+
+        public void setMaxTexture1DLinear(int maxTexture1DLinear) {
+            this.maxTexture1DLinear = maxTexture1DLinear;
+        }
+
+        public int[] getMaxTexture2D() {
+            return maxTexture2D;
+        }
+
+        public void setMaxTexture2D(int[] maxTexture2D) {
+            this.maxTexture2D = maxTexture2D;
+        }
+
+        public int[] getMaxTexture2DMipmap() {
+            return maxTexture2DMipmap;
+        }
+
+        public void setMaxTexture2DMipmap(int[] maxTexture2DMipmap) {
+            this.maxTexture2DMipmap = maxTexture2DMipmap;
+        }
+
+        public int[] getMaxTexture2DLinear() {
+            return maxTexture2DLinear;
+        }
+
+        public void setMaxTexture2DLinear(int[] maxTexture2DLinear) {
+            this.maxTexture2DLinear = maxTexture2DLinear;
+        }
+
+        public int[] getMaxTexture2DGather() {
+            return maxTexture2DGather;
+        }
+
+        public void setMaxTexture2DGather(int[] maxTexture2DGather) {
+            this.maxTexture2DGather = maxTexture2DGather;
+        }
+
+        public int[] getMaxTexture3D() {
+            return maxTexture3D;
+        }
+
+        public void setMaxTexture3D(int[] maxTexture3D) {
+            this.maxTexture3D = maxTexture3D;
+        }
+
+        public int[] getMaxTexture3DAlt() {
+            return maxTexture3DAlt;
+        }
+
+        public void setMaxTexture3DAlt(int[] maxTexture3DAlt) {
+            this.maxTexture3DAlt = maxTexture3DAlt;
+        }
+
+        public int getMaxTextureCubemap() {
+            return maxTextureCubemap;
+        }
+
+        public void setMaxTextureCubemap(int maxTextureCubemap) {
+            this.maxTextureCubemap = maxTextureCubemap;
+        }
+
+        public int[] getMaxTexture1DLayered() {
+            return maxTexture1DLayered;
+        }
+
+        public void setMaxTexture1DLayered(int[] maxTexture1DLayered) {
+            this.maxTexture1DLayered = maxTexture1DLayered;
+        }
+
+        public int[] getMaxTexture2DLayered() {
+            return maxTexture2DLayered;
+        }
+
+        public void setMaxTexture2DLayered(int[] maxTexture2DLayered) {
+            this.maxTexture2DLayered = maxTexture2DLayered;
+        }
+
+        public int[] getMaxTextureCubemapLayered() {
+            return maxTextureCubemapLayered;
+        }
+
+        public void setMaxTextureCubemapLayered(int[] maxTextureCubemapLayered) {
+            this.maxTextureCubemapLayered = maxTextureCubemapLayered;
+        }
+
+        public int getMaxSurface1D() {
+            return maxSurface1D;
+        }
+
+        public void setMaxSurface1D(int maxSurface1D) {
+            this.maxSurface1D = maxSurface1D;
+        }
+
+        public int[] getMaxSurface2D() {
+            return maxSurface2D;
+        }
+
+        public void setMaxSurface2D(int[] maxSurface2D) {
+            this.maxSurface2D = maxSurface2D;
+        }
+
+        public int[] getMaxSurface3D() {
+            return maxSurface3D;
+        }
+
+        public void setMaxSurface3D(int[] maxSurface3D) {
+            this.maxSurface3D = maxSurface3D;
+        }
+
+        public int[] getMaxSurface1DLayered() {
+            return maxSurface1DLayered;
+        }
+
+        public void setMaxSurface1DLayered(int[] maxSurface1DLayered) {
+            this.maxSurface1DLayered = maxSurface1DLayered;
+        }
+
+        public int[] getMaxSurface2DLayered() {
+            return maxSurface2DLayered;
+        }
+
+        public void setMaxSurface2DLayered(int[] maxSurface2DLayered) {
+            this.maxSurface2DLayered = maxSurface2DLayered;
+        }
+
+        public int getMaxSurfaceCubemap() {
+            return maxSurfaceCubemap;
+        }
+
+        public void setMaxSurfaceCubemap(int maxSurfaceCubemap) {
+            this.maxSurfaceCubemap = maxSurfaceCubemap;
+        }
+
+        public int[] getMaxSurfaceCubemapLayered() {
+            return maxSurfaceCubemapLayered;
+        }
+
+        public void setMaxSurfaceCubemapLayered(int[] maxSurfaceCubemapLayered) {
+            this.maxSurfaceCubemapLayered = maxSurfaceCubemapLayered;
+        }
+
+        public long getSurfaceAlignment() {
+            return surfaceAlignment;
+        }
+
+        public void setSurfaceAlignment(long surfaceAlignment) {
+            this.surfaceAlignment = surfaceAlignment;
+        }
+
+        public int getConcurrentKernels() {
+            return concurrentKernels;
+        }
+
+        public void setConcurrentKernels(int concurrentKernels) {
+            this.concurrentKernels = concurrentKernels;
+        }
+
+        public int getECCEnabled() {
+            return ECCEnabled;
+        }
+
+        public void setECCEnabled(int ECCEnabled) {
+            this.ECCEnabled = ECCEnabled;
+        }
+
+        public int getPciBusID() {
+            return pciBusID;
+        }
+
+        public void setPciBusID(int pciBusID) {
+            this.pciBusID = pciBusID;
+        }
+
+        public int getPciDeviceID() {
+            return pciDeviceID;
+        }
+
+        public void setPciDeviceID(int pciDeviceID) {
+            this.pciDeviceID = pciDeviceID;
+        }
+
+        public int getPciDomainID() {
+            return pciDomainID;
+        }
+
+        public void setPciDomainID(int pciDomainID) {
+            this.pciDomainID = pciDomainID;
+        }
+
+        public int getTccDriver() {
+            return tccDriver;
+        }
+
+        public void setTccDriver(int tccDriver) {
+            this.tccDriver = tccDriver;
+        }
+
+        public int getAsyncEngineCount() {
+            return asyncEngineCount;
+        }
+
+        public void setAsyncEngineCount(int asyncEngineCount) {
+            this.asyncEngineCount = asyncEngineCount;
+        }
+
+        public int getUnifiedAddressing() {
+            return unifiedAddressing;
+        }
+
+        public void setUnifiedAddressing(int unifiedAddressing) {
+            this.unifiedAddressing = unifiedAddressing;
+        }
+
+        public int getMemoryClockRate() {
+            return memoryClockRate;
+        }
+
+        public void setMemoryClockRate(int memoryClockRate) {
+            this.memoryClockRate = memoryClockRate;
+        }
+
+        public int getMemoryBusWidth() {
+            return memoryBusWidth;
+        }
+
+        public void setMemoryBusWidth(int memoryBusWidth) {
+            this.memoryBusWidth = memoryBusWidth;
+        }
+
+        public int getL2CacheSize() {
+            return l2CacheSize;
+        }
+
+        public void setL2CacheSize(int l2CacheSize) {
+            this.l2CacheSize = l2CacheSize;
+        }
+
+        public int getMaxThreadsPerMultiProcessor() {
+            return maxThreadsPerMultiProcessor;
+        }
+
+        public void setMaxThreadsPerMultiProcessor(int maxThreadsPerMultiProcessor) {
+            this.maxThreadsPerMultiProcessor = maxThreadsPerMultiProcessor;
+        }
+
+        public int getStreamPrioritiesSupported() {
+            return streamPrioritiesSupported;
+        }
+
+        public void setStreamPrioritiesSupported(int streamPrioritiesSupported) {
+            this.streamPrioritiesSupported = streamPrioritiesSupported;
+        }
+
+        public int getGlobalL1CacheSupported() {
+            return globalL1CacheSupported;
+        }
+
+        public void setGlobalL1CacheSupported(int globalL1CacheSupported) {
+            this.globalL1CacheSupported = globalL1CacheSupported;
+        }
+
+        public int getLocalL1CacheSupported() {
+            return localL1CacheSupported;
+        }
+
+        public void setLocalL1CacheSupported(int localL1CacheSupported) {
+            this.localL1CacheSupported = localL1CacheSupported;
+        }
+
+        public long getSharedMemPerMultiprocessor() {
+            return sharedMemPerMultiprocessor;
+        }
+
+        public void setSharedMemPerMultiprocessor(long sharedMemPerMultiprocessor) {
+            this.sharedMemPerMultiprocessor = sharedMemPerMultiprocessor;
+        }
+
+        public int getRegsPerMultiprocessor() {
+            return regsPerMultiprocessor;
+        }
+
+        public void setRegsPerMultiprocessor(int regsPerMultiprocessor) {
+            this.regsPerMultiprocessor = regsPerMultiprocessor;
+        }
+
+        public int getManagedMemory() {
+            return managedMemory;
+        }
+
+        public void setManagedMemory(int managedMemory) {
+            this.managedMemory = managedMemory;
+        }
+
+        public int getIsMultiGpuBoard() {
+            return isMultiGpuBoard;
+        }
+
+        public void setIsMultiGpuBoard(int isMultiGpuBoard) {
+            this.isMultiGpuBoard = isMultiGpuBoard;
+        }
+
+        public int getMultiGpuBoardGroupID() {
+            return multiGpuBoardGroupID;
+        }
+
+        public void setMultiGpuBoardGroupID(int multiGpuBoardGroupID) {
+            this.multiGpuBoardGroupID = multiGpuBoardGroupID;
+        }
+    
+    
+    
+    
     }
+    
+    
+    
 }
